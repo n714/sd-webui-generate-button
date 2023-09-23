3779a84778af8207707a908214a8ec180087202d
n714 <45053630+n714@users.noreply.github.com>
2023-09-22 17:01:46 -0700

    Add files via upload

---
+++ javascript/generater_button.js
@@ -0,0 +1,20 @@
+﻿class generateButton {
+ static result() {
+     ['txt', 'img'].forEach((mode) => {
+         const generate = document.getElementById(mode + '2img_generate')
+         const box = document.getElementById('image_buttons_' + mode + '2img')
+         const btn = generate.cloneNode()
+            btn.addEventListener('click', () => {
+                generate.dispatchEvent(new Event('click'))
+            })
+            btn.innerHTML = 'Generate'
+            btn.style.position = 'relative'
+            btn.style.minHeight = '2em'
+            btn.style.width = '100%'
+            box.appendChild(btn)
+        })
+    }
+}
+onUiLoaded(async () => {
+    generateButton.result()
+})
